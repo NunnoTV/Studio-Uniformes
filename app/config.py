a42d338893f0069744174c8c21e939db6c14f0f0
Brunno Odlinari <brunnoodlinari@gmail.com>
2025-02-11 22:16:06 -0300

    add configuration classes and Vercel rewrites

---
+++ app/config.py
@@ -0,0 +1,13 @@
+
+import os
+
+class Config:
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.environ.get("SECRET_KEY", "chave-secreta")
+    
+class DevelopmentConfig(Config):
+    DEBUG = True
+    
+class TestingConfig(Config):
+    TESTING = True
