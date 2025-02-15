f20fa9291423ed809357fc135e2552009e3b9dac
Brunno Odlinari <brunnoodlinari@gmail.com>
2025-02-15 16:15:02 -0300

    restruturação de pastas

---
+++ app/api/index.py
@@ -0,0 +1,36 @@
+from flask import Flask
+import os
+from flask import request, jsonify, send_file
+from constants import COMPRESSED_FILE
+from services import image_service
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def diagramar():
+    try:
+        dados = request.get_json()
+        lista = dados.get("lista")
+        url_image = dados.get("imagem")
+        
+        # Processa a imagem e gera o arquivo compactado
+        image_service.process_image_request(lista, url_image)
+        
+        return jsonify({"message": "Arquivo diagramado com sucesso"}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/download", methods=["GET"])
+def download():
+    # Retorna o arquivo compactado para download
+    return send_file(COMPRESSED_FILE, as_attachment=True)
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(debug=True, host="0.0.0.0", port=port)
