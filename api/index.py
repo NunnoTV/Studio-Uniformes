3bacd6aa356e44991033596de3f85df08595c2dc
Brunno Odlinari <brunnoodlinari@gmail.com>
2025-02-17 18:51:04 -0300

    testes vercel

--- api/index.py
+++ api/index.py
@@ -1,34 +1,15 @@
 from flask import Flask
 import os
-from flask import request, jsonify, send_file
-from constants import COMPRESSED_FILE
-from services import image_service
-
-
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-@app.route("/", methods=["POST"])
-def diagramar():
-    try:
-        dados = request.get_json()
-        lista = dados.get("lista")
-        url_image = dados.get("imagem")
-        
-        # Processa a imagem e gera o arquivo compactado
-        image_service.process_image_request(lista, url_image)
-        
-        return jsonify({"message": "Arquivo diagramado com sucesso"}), 200
-    
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-@app.route("/download", methods=["GET"])
-def download():
-    # Retorna o arquivo compactado para download
-    return send_file(COMPRESSED_FILE, as_attachment=True)
-
+@app.route('/about')
+def about():
+    return 'About'
 
 
 if __name__ == "__main__":
